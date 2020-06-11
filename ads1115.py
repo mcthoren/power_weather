e0406212ba15a2ae9221b78fca0b960eda90da4f
mcthoren <thoren.mcdole@gmail.com>
2020-06-11 15:36:22 +0000

    timestamps, first stab at power calculation, labels

--- ads1115.py
+++ ads1115.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 # https://www.adafruit.com/product/1085
 
-import board, busio, time
+import board, busio, time, datetime
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
@@ -17,13 +17,14 @@ c1 = AnalogIn(ads, ADS.P1)
 c2 = AnalogIn(ads, ADS.P2)
 c3 = AnalogIn(ads, ADS.P3)
 
+ads.gain = 1
+
 if __name__ == "__main__":
 	while True:
 
 		for i in range(0, 4):
 			p_vals[i] = vals[i] = 0
 
-		time0 = time1 = time.time()
 		for i in range(0, 150):
 			vals[0] = c0.voltage
 			vals[1] = c1.voltage
@@ -33,11 +34,15 @@ if __name__ == "__main__":
 			# "peak detection" python's not really fast enough to do this.
 			for i in range(0, 4):
 				if vals[i] > p_vals[i]:
-					 p_vals[i] = vals[i]
+					p_vals[i] = vals[i]
 
-		time1 = time.time()
 
-		dat_s = "0:\t%.4f V, 1:\t%.2f V, 2:\t%.2f V, 3:\t%.2f V" % (p_vals[0], p_vals[1], p_vals[2], p_vals[3])
+		ts = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+
+		# in theory, if we measure peak to peak AC voltage with a current probe that
+		# reads 1V for every 15A, and if our voltage and current are in phase. then:
+		# rms power [W] = reading [V] * sqrt(2) * line voltage [V] * 15[A/V]
+		theory_power = p_vals[0] * .707106781 * 229 * 15
+		# dat_s = "%s\t0:\t%.4f V, 1:\t%.2f V, 2:\t%.2f V, 3:\t%.2f V" % (ts, p_vals[0], p_vals[1], p_vals[2], p_vals[3])
+		dat_s = "%s\tph0: %.4f V,\tBatt: %.2f V,\tPower: %.0f W" % (ts, p_vals[0], p_vals[1], theory_power)
 		print(dat_s)
-		d_t = "%.2f" % (time1 - time0)
-		print(d_t)
