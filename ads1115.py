f502d0bc6c769bde101c92e4ae4aefe0752aa016
mcthoren <thoren.mcdole@gmail.com>
2020-06-12 11:14:27 +0000

    hook out file output

--- ads1115.py
+++ ads1115.py
@@ -2,15 +2,21 @@
 # -*- coding: UTF-8 -*-
 # https://www.adafruit.com/product/1085
 
-import board, busio, time, datetime
+import board, busio, time, datetime, sys
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+
+sys.path.append('/home/ghz/wxlib')
+import wxlib as wx
 
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 
 vals = {}
 p_vals = {}
+
+dat_fname='power'
+wx_dir = "/home/ghz/power_wx"
 
 c0 = AnalogIn(ads, ADS.P0)
 c1 = AnalogIn(ads, ADS.P1)
@@ -44,5 +50,6 @@ if __name__ == "__main__":
 		# rms power [W] = current probe reading [Vpp] * sqrt(2) * line voltage [V] * 15[A/V]
 		theory_power = p_vals[0] * .707106781 * 229 * 15
 		# dat_s = "%s\t0:\t%.4f V, 1:\t%.2f V, 2:\t%.2f V, 3:\t%.2f V" % (ts, p_vals[0], p_vals[1], p_vals[2], p_vals[3])
-		dat_s = "%s\tph0: %.4f V,\tBatt: %.2f V,\tPower: %.0f W" % (ts, p_vals[0], p_vals[1], theory_power)
-		print(dat_s)
+		dat_s = "%s\tph0: %.4f V,\tBatt: %.2f V,\tPower: %.0f W\n" % (ts, p_vals[0], p_vals[1], theory_power)
+		wx.write_out_dat_stamp(ts, dat_fname, dat_s, wx_dir)
+		print(dat_s, end='', flush=True)
